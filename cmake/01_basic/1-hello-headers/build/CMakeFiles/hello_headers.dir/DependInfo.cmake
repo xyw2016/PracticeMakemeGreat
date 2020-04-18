2e2c1b6e0111d1cdd9035f502fbda46ef7b4c764
Xiang-Yu Wu <xiangyuwu@mails.ccnu.edu.cn>
2020-04-19 03:34:41 +0800

    cmake/basic/hello and hello_headers

---
+++ cmake/01_basic/1-hello-headers/build/CMakeFiles/hello_headers.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/xyw/windows/linuxfile/code/keeping_learning/cmake/01_basic/1-hello-headers/src/hello.cpp" "/media/xyw/windows/linuxfile/code/keeping_learning/cmake/01_basic/1-hello-headers/build/CMakeFiles/hello_headers.dir/src/hello.cpp.o"
+  "/media/xyw/windows/linuxfile/code/keeping_learning/cmake/01_basic/1-hello-headers/src/main.cpp" "/media/xyw/windows/linuxfile/code/keeping_learning/cmake/01_basic/1-hello-headers/build/CMakeFiles/hello_headers.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
