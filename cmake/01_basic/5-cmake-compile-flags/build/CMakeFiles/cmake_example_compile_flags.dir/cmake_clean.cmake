39fd146e4372defa06c69dbfe311dc3f23d4c5e2
Xiang-Yu Wu <xiangyuwu@mails.ccnu.edu.cn>
2020-04-21 05:49:21 +0800

    cmake basic cmake-compile-flags and third-party-library

---
+++ cmake/01_basic/5-cmake-compile-flags/build/CMakeFiles/cmake_example_compile_flags.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_example_compile_flags.dir/main.cpp.o"
+  "cmake_example_compile_flags.pdb"
+  "cmake_example_compile_flags"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_example_compile_flags.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake/01_basic/6-third-party-library/build/CMakeFiles/third_party_include.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/third_party_include.dir/main.cpp.o"
+  "third_party_include.pdb"
+  "third_party_include"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/third_party_include.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
