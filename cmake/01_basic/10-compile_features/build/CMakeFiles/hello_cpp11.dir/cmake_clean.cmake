7a4593554b69fa68a75ea65097718c4c19d7a74b
Xiang-Yu Wu <xiangyuwu@mails.ccnu.edu.cn>
2020-04-26 22:39:32 +0800

    cmake basic 3 kind of methods to select the version of c++

---
+++ cmake/01_basic/10-compile_features/build/CMakeFiles/hello_cpp11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cpp11.dir/main.cpp.o"
+  "hello_cpp11.pdb"
+  "hello_cpp11"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cpp11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
